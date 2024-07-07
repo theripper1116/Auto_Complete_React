bb01a93ae13c2261602be123493f28e6ee708160
theripper1116 <milanagarwallaj@yahoo.in>
2024-07-07 18:13:18 +0530

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div className="mt-[10%]">
+        <div className="mx-[45%]">Search Bar</div>
+        <input className="mx-[45%] border-black"></input>
+        <div className="mx-[45%]">Search Results</div>
+      </div>
+    </>
+  );
+}
+
+export default App;
